5284b4839100d5bccee3dceee8c9586aecef7609
HDTian <127906571+HDTian@users.noreply.github.com>
2023-04-18 18:57:20 +0200

    Upload

---
+++ R/Smooth.R
@@ -0,0 +1,254 @@
+
+#My smoothing for LACEs
+
+#Concepts: internal knots; order ; Roughness penalty; random-effect setting
+
+
+#For piecewise: internal knots use stratum min/max boundary + Noder = 1
+#For polynomial: higher order (=1, 2, ...) and not use knots
+
+
+Smooth<-function( res,#RES$DRres #the result summary information table RES<-getSummaryInf(rdat)
+                  Rall=NA, #A vector: Exposure range for smoothing and visualization
+                  baseline=NA, #the baseline exposure value for orginal causal effect shape
+                  Norder=1,
+                  Knots=NA, #internal knots;  c(1,2,3) or '3' (character)
+                  Lambda=0,  #tuning parameter for roughness; lambda=0 means no roughness penalty
+                  random_effect=TRUE,
+                  getHeterQ=TRUE, #if to show the Q p-value for the differentiate function plot?
+                  Plot=FALSE  #logic; Plot=TRUE will automatically print the two ggplots
+){
+  if(getHeterQ){
+    Sr<-mr_ivw(mr_input(res$bx, res$bxse ,res$by, res$byse))#naive IVW under null (no effect heterogeneity)
+    #mr_ivw是免疫NA的，即，即使把NA移除掉，naive IVW estimate依旧是一样的
+    delta<-Sr@Estimate #一维的
+    #Cochran's Q statistic
+    ssigma_square <- res$byse^2 + delta^2*res$bxse^2
+    QQ<-sum( ( res$by - delta*res$bx     )^2 /ssigma_square ,na.rm=TRUE      )
+    #critical value:
+    Qdf<-sum(!is.na( ( res$by - delta*res$bx     )^2 /ssigma_square  )) -1 #normally Ns-1 without NA stratum
+    pvalue<-1-pchisq(QQ, Qdf ) #不会考虑NA的stratum
+    HeterQ<-c(QQ, Qdf, round(pvalue,3)  )#; names(HeterQ)<-c('Q statistic' ,'df' ,'p-value'  )
+    #RES$RHeterQ<-HeterQ
+  }
+
+  SmoothRes<-list()
+  #get the minimal useful information table
+  RES<-as.data.frame(cbind( res$mean,res$est , res$se  ))
+  names(RES)<-c('mean', 'est', 'se'  )
+  #romove the NA values
+  RES<-na.omit(RES)
+  Ns<-nrow(RES) #the number of stratum after removing NA values
+
+  if(is.na(Rall)){
+    Rall<-c(min( RES$mean)-1, max(RES$mean)+1    )
+    cat('The exposure range used: ', Rall,'\n')}
+
+  if(is.vector(Knots)){
+    internal_knots<-sort(Knots)  #sort(NA) -> logical(0); 会被breaks这个vector自动忽略
+    if( sum( !c(Rall[1]<internal_knots,internal_knots<Rall[2]) )>0 ){
+      stop('make sure that the internal knots are inside the Rall')
+    }
+  }
+
+  if(is.character(Knots)){
+    if(as.numeric(Knots)-round(as.numeric(Knots))==0  ){
+      internal_knots<-seq(Rall[1],Rall[2],len=as.numeric(Knots)+2)[-c(1,as.numeric(Knots)+2)]
+      }else{
+      stop('PLease use the correctly integer number of Knots')
+    }
+  }
+  cat('The internal knots are: ', internal_knots,' (Empty means no internal knots used)','\n')
+
+  Bs<-create.bspline.basis(  c(Rall[1],Rall[2])  ,
+                             breaks=c(  Rall[1],  internal_knots ,  Rall[2] ) ,
+                             norder=Norder )
+  Par <- fdPar( fdobj=Bs, Lfdobj=NULL, lambda=Lambda )#都默认不用penalty;这里应该没问�?
+  smoothed_LACE <- smooth.basis(  RES$mean  , RES$est , Par ,wtvec=1/RES$se^2 )$fd
+  dsdf<-smooth.basis(  RES$mean  , RES$est , Par ,wtvec=1/RES$se^2 )$df  # = # of interor knots + order
+  thetahat<-smoothed_LACE$coefs
+  SmoothRes$thetahat<-as.vector(thetahat)
+  ###use my code with SLR, no black box
+  #SSigma<-diag(  1/RES$se )#standard (non-squared) sigma; i.e. Sigma^{-1/2}
+  #yy<-SSigma%*%RES$est
+  MMM<- eval.basis(   RES$mean , Bs      )  #function values at the  RES$mean position (mean_exposure observed position)
+  #XX<-SSigma%*%MMM
+  #SLRthetahat<-solve(  t( XX)%*%XX )%*% t( XX)%*% yy
+  #clear! 关键在于wtvec需要使用平方量�?
+  ###接着估计tau^2�?
+  #直接用原始的weighted regression - LRT:
+  temp<-MMM%*%thetahat# fitted values
+  if(random_effect){
+    if(Ns==dsdf){
+      tau2<-1   #这里主要是考虑到完全full model的情�?(即，fractional polynomial method)，此时df分母=0 容易报错
+    }else{
+      tau2<-max(1,sum(  (RES$est-temp)^2/RES$se^2     )/(  Ns- dsdf))#没有roughness penalty，那df自然是整�?
+      }
+    cat('Random-effect is considered for smoothing and the actual random effect squared value is: ', tau2,'\n')
+  }else{
+    tau2<-1
+  }
+
+  #or: use Simple Linear Regression
+  #tau2<-max(1,sum( (yy-XX%*%SLRthetahat )^2 )/(Ns -dsdf))#checked 确实是一样的
+
+
+  ###Following inference
+  time_obs<-seq(Rall[1],Rall[2],len=1000) #visualization points
+
+  ###WLR with fda code
+  y2cMap<- smooth.basis(  RES$mean  , RES$est , Par ,wtvec=1/RES$se^2 )$y2cMap
+  varthetahat<-y2cMap %*% diag(  RES$se^2 )  %*% t(y2cMap  )* tau2
+
+  SmoothRes$var.matrix<-varthetahat
+  summary_table<-cbind( as.vector(thetahat),
+                        sqrt(as.vector(diag(varthetahat))))  #marginal s.e.s
+  #依据tau2的值来判断使用Z-tes还是t-testt；毕竟tau2==1时，相当于normal variance 不被估计的情�?
+  if(tau2==1){
+    summary_table<-cbind(summary_table,2*(1-pnorm(  abs(as.vector(thetahat)/sqrt(as.vector(diag(varthetahat))))  )   ) )
+  }else{
+    summary_table<-cbind(summary_table,2*(1-pt(  abs(as.vector(thetahat)/sqrt(as.vector(diag(varthetahat))) ), Ns-length(thetahat)   )   )      )
+    }
+  colnames(summary_table)<-c('est','s.e.','p-value')
+  rownames(summary_table)<-paste0('Par',1:length(thetahat))
+  SmoothRes$summary<-round(summary_table,3)
+  #y2cMap表达式上就是solve(  t( XX)%*%XX )%*% t( XX)%*%SSigma；含义就是WLR的hat matrix
+  MM<- eval.basis(  time_obs , Bs      )  #matrix of basis function values in all positions
+  #dim(MM) #1000 dsdf #df=degree-of-freedom= # of theta parameters
+  varM<- MM %*% varthetahat %*%  t(  MM)  #最后一向是random-effect
+
+  pw_std_error<-sqrt( diag(varM) )  #由于是Pointwise, 只取对角线元素即�?
+  smoothed_LACE_<-eval.fd(    time_obs ,smoothed_LACE )
+
+  # plot(time_obs,smoothed_LACE_,type='l')
+  # lines(time_obs , smoothed_LACE_+1.96*pw_std_error, lty=2, lwd=1        )
+  # lines( time_obs, smoothed_LACE_-1.96*pw_std_error, lty=2, lwd=1          )
+
+  ggdata<-data.frame( tps=RES$mean, est = RES$est,
+                      estlow=RES$est -1.96*RES$se,
+                      estup=RES$est +1.96*RES$se
+  )
+  ggsmooth<-data.frame( time_obs=time_obs,
+                        smoothed_LACE_=smoothed_LACE_,
+                        smooth_low=smoothed_LACE_-1.96*pw_std_error,
+                        smooth_up=smoothed_LACE_+1.96*pw_std_error)
+  if(getHeterQ){
+    p<-ggplot(ggdata, aes(tps, est))+
+      geom_point(data=ggdata,mapping=aes(x=tps,y=est),color='black',alpha=1)+
+      geom_errorbar(data=ggdata,mapping=aes(x=tps,ymin=estlow,ymax=estup),color='black',width = 0.1)+
+      geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smoothed_LACE_),color='black',alpha=1)+
+      geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smooth_low),color='black',alpha=1,linetype='dashed')+
+      geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smooth_up),color='black',alpha=1,linetype='dashed')+
+      geom_hline( yintercept=0, color='grey',linetype='dashed')+
+      labs(x='Exposure level',y='LACE estimates',subtitle =paste0("Stratification non-linearity p-value: ",HeterQ[3]) )+
+      theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+      coord_cartesian(ylim = c( min(res$est-1.96*res$se)-0.5,max(res$est+1.96*res$se)+0.5) )
+  }else{
+    p<-ggplot(ggdata, aes(tps, est))+
+      geom_point(data=ggdata,mapping=aes(x=tps,y=est),color='black',alpha=1)+
+      geom_errorbar(data=ggdata,mapping=aes(x=tps,ymin=estlow,ymax=estup),color='black',width = 0.1)+
+      geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smoothed_LACE_),color='black',alpha=1)+
+      geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smooth_low),color='black',alpha=1,linetype='dashed')+
+      geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smooth_up),color='black',alpha=1,linetype='dashed')+
+      geom_hline( yintercept=0, color='grey',linetype='dashed')+
+      labs(x='Exposure level',y='LACE estimates' )+
+      theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+      coord_cartesian(ylim = c( min(res$est-1.96*res$se)-0.5,max(res$est+1.96*res$se)+0.5) )
+  }
+
+  if(Plot){print(p)}
+  SmoothRes$p<-p
+
+
+
+
+
+
+
+
+  ###SLR with my code
+  # varSLRthetahat<-solve(  t( XX)%*%XX )*tau2
+  # varM<- MM %*% varSLRthetahat %*%  t(  MM)
+  # pw_std_error<-sqrt( diag(varM) )  #由于是Pointwise, 只取对角线元素即�?
+  # smoothed_LACE_<-eval.fd(    time_obs ,smoothed_LACE )
+  # plot(time_obs,smoothed_LACE_,type='l')
+  # lines(time_obs , smoothed_LACE_+1.96*pw_std_error, lty=2, lwd=1        )
+  # lines( time_obs, smoothed_LACE_-1.96*pw_std_error, lty=2, lwd=1          )
+  # #已经check；确实是一模一�?
+
+  ###For original effect shape
+  if(is.na(baseline)){baseline<-RES$mean[1]}
+  baseline_used<-time_obs[sum(time_obs<=baseline)]
+  cat('The actual basline value used:',baseline_used ,'\n')
+
+
+  DD<-apply( MM, 2, cumsum )*(Rall[2]-Rall[1] )/(1000-1)  #\int_{Rall[1]}^x theta(s)ds where x=time_obs
+  BaseInt<-matrix(rep( DD[sum(time_obs<=baseline),],1000  ), byrow=TRUE,nrow=1000 ) #int_{Rall[1]}^{baseline_used~+} theta(s)ds   #保证baseline_used那一行DD�?0
+  DD<-DD-BaseInt
+
+  hest<-DD%*%thetahat  #h(x) pointwise est
+
+  varD<- DD %*% varthetahat %*%  t(  DD)  #最后一向是random-effect
+  pw_std_error<-sqrt( diag(varD) )  #由于是Pointwise, 只取对角线元素即�?
+
+  # plot(time_obs,hest,type='l', xlab='exposure level'  , ylab='h(x)')
+  # lines(time_obs , hest+1.96*pw_std_error, lty=2, lwd=1        )
+  # lines( time_obs, hest-1.96*pw_std_error, lty=2, lwd=1          )
+  # abline(  h=0  ,lty=2, col ='blue' )
+  # abline(  v=baseline_used  ,lty=2, col ='blue' )
+  #checked, 两种DD的构造方法的可视化结果几乎一模一样！
+
+  ggsmooth<-data.frame( time_obs=time_obs,
+                        hest=hest,
+                        smooth_low=hest-1.96*pw_std_error,
+                        smooth_up=hest+1.96*pw_std_error)
+  hp<-ggplot(ggdata, aes(tps, est))+
+    geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=hest),color='black',alpha=1)+
+    geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smooth_low),color='black',alpha=1,linetype='dashed')+
+    geom_line(data=ggsmooth,mapping=aes(x=time_obs,y=smooth_up),color='black',alpha=1,linetype='dashed')+
+    geom_hline( yintercept=0, color='grey',linetype='dashed')+
+    geom_vline( xintercept=baseline_used, color='grey',linetype='dashed')+
+    labs(x='Exposure level',y='Effect')+
+    theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+    coord_cartesian(ylim = c( min(hest)-1,max(hest)+1) )
+  if(Plot){print(hp)}
+  SmoothRes$hp<-hp
+
+  return(SmoothRes)
+
+}
+
+
+##example
+
+
+# dat<-getDat( IVtype='cont', ZXmodel='A',XYmodel='2' ) #get a toy data
+# rdat<-Stratify(dat)  #Do stratification on the data
+# RES<-getSummaryInf( rdat,XYmodel='2')
+#
+# #fractional polynomial method with df=2
+# smooth_res<-Smooth(RES$DRres,Norder=3,baseline=0)  #smoothing
+# smooth_res$summary #B-spline basisfunctions parameters and s.e. p-values
+# smooth_res$p #h'(x) function
+# smooth_res$hp  #h(x) function
+#
+#
+#
+# #piecewise linear method
+# cutting_values<-(RES$DRres$mean[-1] + head( RES$DRres$mean,-1) )/2
+#
+#
+# smooth_res<-Smooth(RES$DRres,Norder=1,baseline=0,Knots=cutting_values)  #smoothing
+# smooth_res$summary #B-spline basisfunctions parameters and s.e. p-values
+# smooth_res$p #h'(x) function
+# smooth_res$hp
+
+
+
+
+
+
+
+
+
+
---
+++ R/getSummaryInf.R
@@ -0,0 +1,249 @@
+
+
+
+
+
+getSummaryInf<-function(rdat, #rdat #rdat<-Stratify(dat)   i.e. stratified individual dataset
+                        target=TRUE, #if TRUE, will calculate the target causal effect for each stratum
+                        XYmodel='1', #tell me what the true X-Y effect shape is? only applicable when target=TRUE #XYmodel='0' : use self-defined effect shape, in global environment must define:  dif()
+                        bxthre=1e-5, #bx threshold, under this value the bx is regarded as 0 or NA (so no MR ests)
+                        getHeterQ=TRUE,  #get the heterogeneity Q statistics?
+                        onlyDR=FALSE #only show the DR stratification results?
+  ){
+
+  #dat check
+  if( is.null(rdat$Y) ){stop('No Y detected!: The column of the outcome should be named as Y') }
+
+
+  Ns<-length(  levels(factor(rdat$DRstratum))  )
+  stratify_on_X_or_not<-sum(rdat$M==rdat$X)==nrow(rdat)
+
+
+  RES<-list()
+
+
+  if(target&( !stratify_on_X_or_not ) ){
+    stop('The target is not applicable as the stratified variable is not the exposure itself;
+        Try target=FALSE or stratify on the exposure instead')
+  }
+
+
+  if( target&stratify_on_X_or_not&(!XYmodel%in%c('1','2','3'))    ){
+    if( exists('dif')){
+      print('The (derivative) causal function is')
+      print( dif )
+      }else{ stop('Pleas define the effect function (dif) or use correct XYmodel index')   }
+  }
+
+  ##now assign the nonlinear_dif
+  if( !XYmodel%in%c('1','2','3') ){   nonlinear_dif<-dif      }
+  if( XYmodel=='1' ){    nonlinear_dif<-function(t){ sapply(   t , function(tt){ 0}) }     }
+  if( XYmodel=='2' ){    nonlinear_dif<-function(t){0.2*t}   }
+  if( XYmodel=='3' ){    nonlinear_dif<-function(t){-0.2*t*(t>0)}    }
+
+
+  if( is.null(rdat$X_) ){ rdat$X_<-rdat$X  } #Target effect is the expected stratum-specific effect without any error (either confounding or coarsened error)
+
+
+
+
+
+  if(!onlyDR){
+    ##Residual stratification----------------------------------------------------------
+    ##---------------------------------------------------------------------------------
+    Size<-c()
+    Minf<-c()  #M (either exposure or covariate) information
+    MRfitting<-c()
+    Target<-c()
+    for(i in 1:Ns){
+      selected_dat<-rdat[ rdat$Rstratum==i,  ]
+      ##Stratum size
+      Size<-c( Size , nrow(selected_dat))
+
+      ##M information (min, mean, max)
+      Minf<-rbind( Minf, as.numeric(summary(selected_dat$M   ))[c(1,2,4,5,6)]   )
+
+
+      if( length(table(selected_dat$Z))==1  ){#如果Z都是单值的，谈何IV analysis?
+        MRfitting<-rbind(MRfitting, c(NA,NA,NA,NA,NA,NA)); Target<-c(Target ,   NA)
+      }else{
+        ##naive IVW MR est
+        fitGX<-lm(    selected_dat$X~  selected_dat$Z  );bGX<-summary(fitGX)$coef[-1,1] ;seGX<- summary(fitGX)$coef[-1,2] ; bx<-as.numeric( bGX  ); bxse<-as.numeric(  seGX)
+        fitGY<-lm(    selected_dat$Y~  selected_dat$Z  );bGY<-summary(fitGY)$coef[-1,1] ;seGY<-summary(fitGY)$coef[-1,2] ; by<-as.numeric( bGY  ); byse<-as.numeric(  seGY)
+        if( length(bx)==0  ){#即，bx是numeric(0)；这并没有说明存在null G-X的情况，而是因为Z是单一值的
+          MRfitting<-rbind(MRfitting, c(NA,NA,by,byse,NA,NA))
+        }else{
+          if(abs(bx)<bxthre ){  #即，bx过小，可能产生inf项目，所以此时直接用NA代替
+            MRfitting<-rbind(MRfitting, c(NA,NA,by,byse,NA,NA))
+          }else{
+            MRres<-mr_ivw(mr_input(bx, bxse, by, byse))
+            MRfitting<-rbind(MRfitting, c(bx,bxse,by,byse,MRres@Estimate,MRres@StdError))
+          }
+        }
+
+
+
+        ##Target causal effect (只有M==X时，算这个才有意义)
+        #注意target是按照X求还是X_求？还是按照X_求吧
+        if(target){
+          #my definition
+          At<-function(t){
+            cov(     selected_dat$Z    , as.numeric(  selected_dat$X_ >= t  ) )/var( selected_dat$Z )
+          }
+          RRR<-c(min(selected_dat$X_  ),   max( selected_dat$X_    ) )  #RRR range for FDA
+          time_obs<- seq( RRR[1],RRR[2], len=1000 )#length越大越精准
+          At_<-function(t){sapply(t, function(t){ At(t)} ) } #At函数可积化
+          Aobs<- At_(   time_obs )
+          #B-spline smoothing
+          Bs<-create.bspline.basis(  c(0,RRR[2]-RRR[1])  , nbasis=54 )  #50个interior knots (m默认均匀分布); order=4 by default
+          Par <- fdPar( fdobj=Bs, Lfdobj=NULL, lambda=0 )
+          smoothed_fd <- smooth.basis(  time_obs-RRR[1]  , Aobs  , Par  )$fd
+          smoothed_At<-eval.fd(smoothed_fd, seq(0,RRR[2]-RRR[1] , len=1000 )   )
+          #最终就是获得At这条曲线在time_obs点上的值，方便积分而已
+          #其实使用FDA没有太大必要，
+
+          hdx<-nonlinear_dif( time_obs      ) #h'(x)在time_obs上的值
+          times<-smoothed_At*hdx  #At*hdx 在time_obs上的值
+          intres<-int.simpson2( time_obs ,times, equi = TRUE, method = NULL)  #integration result
+          #intres<-int.simpson2( seq(0,RRR[2]-RRR[1] , len=1000 ) ,times, equi = TRUE, method = NULL)  #integration result
+          #intres<-int.simpson2( time_obs ,Aobs*hdx , equi = TRUE, method = NULL)
+
+          fitGX_<-lm(    selected_dat$X_~  selected_dat$Z  );bx_<-as.numeric(summary(fitGX_)$coef[-1,1])
+          targetvalue<-intres/bx_  #target effect  (除以bx是因为之前的At分母并不是cov(Z,X)) #bx_是依据X_算的；即，可能coarsened
+          Target<-c(Target ,   targetvalue)
+        }else{Target<-c(Target ,   NA)}
+
+      }
+
+
+
+
+    }
+
+    res<-cbind( Size,Minf, MRfitting , Target)
+    res<-as.data.frame(res)
+    colnames(res)<-c('size', 'min','1q','mean', '3q', 'max','bx','bxse','by','byse','est','se','target')
+    rownames(res)<-paste0('Stratum ', 1:Ns  )
+
+    RES$Rres<-res
+
+    if(getHeterQ){
+      Sr<-mr_ivw(mr_input(res$bx, res$bxse ,res$by, res$byse))#naive IVW under null (no effect heterogeneity)
+      #mr_ivw是免疫NA的，即，即使把NA移除掉，naive IVW estimate依旧是一样的
+      delta<-Sr@Estimate #一维的
+      #Cochran's Q statistic
+      ssigma_square <- res$byse^2 + delta^2*res$bxse^2
+      QQ<-sum( ( res$by - delta*res$bx     )^2 /ssigma_square ,na.rm=TRUE      )
+      #critical value:
+      Qdf<-sum(!is.na( ( res$by - delta*res$bx     )^2 /ssigma_square  ))    -1#normally Ns-1 without NA stratum
+      pvalue<-1-pchisq(QQ, Qdf ) #不会考虑NA的stratum
+      HeterQ<-c(QQ, Qdf, pvalue  ); names(HeterQ)<-c('Q statistic' ,'df' ,'p-value'  )
+      RES$RHeterQ<-HeterQ
+    }
+  }
+
+
+  ##Doubly-ranked stratification----------------------------------------------------------
+  ##---------------------------------------------------------------------------------
+  Size<-c()
+  Minf<-c()  #M (either exposure or covariate) information
+  MRfitting<-c()
+  Target<-c()
+  for(i in 1:Ns){
+    selected_dat<-rdat[ rdat$DRstratum==i,  ]
+    ##Stratum size
+    Size<-c( Size , nrow(selected_dat))
+
+    ##M information (min, mean, max)
+    Minf<-rbind( Minf, as.numeric(summary(selected_dat$M   ))[c(1,2,4,5,6)]   )
+    if( length(table(selected_dat$Z))==1  ){#如果Z都是单值的，谈何IV analysis?
+      MRfitting<-rbind(MRfitting, c(NA,NA,NA,NA,NA,NA)); Target<-c(Target ,   NA)
+    }else{
+      ##naive IVW MR est
+      fitGX<-lm(    selected_dat$X~  selected_dat$Z  );bGX<-summary(fitGX)$coef[-1,1] ;seGX<- summary(fitGX)$coef[-1,2] ; bx<-as.numeric( bGX  ); bxse<-as.numeric(  seGX)
+      fitGY<-lm(    selected_dat$Y~  selected_dat$Z  );bGY<-summary(fitGY)$coef[-1,1] ;seGY<-summary(fitGY)$coef[-1,2] ; by<-as.numeric( bGY  ); byse<-as.numeric(  seGY)
+      if( length(bx)==0  ){#即，bx是numeric(0)；说明存在null G-X的情况
+        MRfitting<-rbind(MRfitting, c(NA,NA,by,byse,NA,NA))
+      }else{
+        if(abs(bx)<bxthre ){  #即，bx过小，可能产生inf项目，所以此时直接用NA代替
+          MRfitting<-rbind(MRfitting, c(NA,NA,by,byse,NA,NA))
+        }else{
+          MRres<-mr_ivw(mr_input(bx, bxse, by, byse))
+          MRfitting<-rbind(MRfitting, c(bx,bxse,by,byse,MRres@Estimate,MRres@StdError))
+        }
+      }
+
+      ##Target causal effect (只有M==X时，算这个才有意义)
+      #注意target是按照X求还是X_求？还是按照X_求吧
+      if(target){
+        #my definition
+        At<-function(t){
+          cov(     selected_dat$Z    , as.numeric(  selected_dat$X_ >= t  ) )/var( selected_dat$Z )
+        }
+        RRR<-c(min(selected_dat$X_  ),   max( selected_dat$X_    ) )  #RRR range for FDA
+        time_obs<- seq( RRR[1],RRR[2], len=1000 )#length越大越精准
+        At_<-function(t){sapply(t, function(t){ At(t)} ) } #At函数可积化
+        Aobs<- At_(   time_obs )
+        #B-spline smoothing
+        Bs<-create.bspline.basis(  c(0,RRR[2]-RRR[1])  , nbasis=54 )  #50个interior knots (m默认均匀分布); order=4 by default
+        Par <- fdPar( fdobj=Bs, Lfdobj=NULL, lambda=0 )
+        smoothed_fd <- smooth.basis(  time_obs-RRR[1]  , Aobs  , Par  )$fd
+        smoothed_At<-eval.fd(smoothed_fd, seq(0,RRR[2]-RRR[1] , len=1000 )   )
+        #最终就是获得At这条曲线在time_obs点上的值，方便积分而已
+        #其实使用FDA没有太大必要，
+
+        hdx<-nonlinear_dif( time_obs      ) #h'(x)在time_obs上的值
+        times<-smoothed_At*hdx  #At*hdx 在time_obs上的值
+        intres<-int.simpson2( time_obs ,times, equi = TRUE, method = NULL)  #integration result
+        #intres<-int.simpson2( seq(0,RRR[2]-RRR[1] , len=1000 ) ,times, equi = TRUE, method = NULL)  #integration result
+        #intres<-int.simpson2( time_obs ,Aobs*hdx , equi = TRUE, method = NULL)
+
+        fitGX_<-lm(    selected_dat$X_~  selected_dat$Z  );bx_<-as.numeric(summary(fitGX_)$coef[-1,1])
+        targetvalue<-intres/bx_  #target effect  (除以bx是因为之前的At分母并不是cov(Z,X)) #bx_是依据X_算的；即，可能coarsened
+        Target<-c(Target ,   targetvalue)
+      }else{Target<-c(Target ,   NA)}
+    }
+
+
+  }
+
+  res<-cbind( Size,Minf, MRfitting , Target)
+  res<-as.data.frame(res)
+  colnames(res)<-c( 'size', 'min', '1q','mean','3q','max','bx','bxse','by','byse','est','se','target')
+  rownames(res)<-paste0('Stratum ', 1:Ns  )
+
+  RES$DRres<-res
+
+  if(getHeterQ){
+    Sr<-mr_ivw(mr_input(res$bx, res$bxse ,res$by, res$byse))#naive IVW under null (no effect heterogeneity)
+    #mr_ivw是免疫NA的，即，即使把NA移除掉，naive IVW estimate依旧是一样的
+    delta<-Sr@Estimate #一维的
+    #Cochran's Q statistic
+    ssigma_square <- res$byse^2 + delta^2*res$bxse^2
+    QQ<-sum( ( res$by - delta*res$bx     )^2 /ssigma_square ,na.rm=TRUE      )
+    #critical value:
+    Qdf<-sum(!is.na( ( res$by - delta*res$bx     )^2 /ssigma_square  ))    -1#normally Ns-1 without NA stratum
+    pvalue<-1-pchisq(QQ, Qdf ) #不会考虑NA的stratum
+    HeterQ<-c(QQ, Qdf, pvalue  ); names(HeterQ)<-c('Q statistic' ,'df' ,'p-value'  )
+    RES$DRHeterQ<-HeterQ
+  }
+
+
+  return(RES)
+}
+
+#Warning message:
+#  In summary.lm(fitGX) : essentially perfect fit: summary may be unreliable
+#Z肯定是多值的(否则不会跑到这一条代码)，这其实代表着X是一个值(往往是coarsened导致的)；此时bx为0，
+#代码已经设计了此时会把bx=NA by不用管 MRest=NA MRse=NA; 不用管target，因为target是针对X_，而X_是连续的
+
+
+###examples
+# getSummaryInf(rdat, target=FALSE, bxthre=1e-5)
+#
+#
+#
+# dat<-getDat( IVtype='cont', ZXmodel='C',XYmodel='1' ) #get a toy data
+# rdat<-Stratify(dat)  #Do stratification on the data
+# RES<-getSummaryInf( rdat, target=TRUE, bxthre=1e-5, XYmodel='1',getHeterQ=TRUE)
+
